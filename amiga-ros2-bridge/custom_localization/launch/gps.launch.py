499a8beae4124ebfcb2a836f386c375cdf1d0a7a
TeunGit <afm.lauwerijssen@student.avans.nl>
2026-06-10 11:19:27 +0200

    added custom imu microros node

--- amiga-ros2-bridge/custom_localization/launch/gps.launch.py
+++ amiga-ros2-bridge/custom_localization/launch/gps.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
@@ -13,7 +13,8 @@ import os
 def generate_launch_description():
 
     pkg_dir = get_package_share_directory('custom_localization')
-
+    imu_port = LaunchConfiguration('imu_port')
+    imu_baud = LaunchConfiguration('imu_baud')
 
     navsat_file = os.path.join(
         pkg_dir,
@@ -40,6 +41,16 @@ def generate_launch_description():
             "gps_frame",
             default_value="lg580p_link"
         ),
+
+        DeclareLaunchArgument(
+            "imu_port",
+            default_value="/dev/ttyUSB0"
+        ),
+         DeclareLaunchArgument(
+            "imu_baud",
+            default_value="115200"
+        ),
+
         DeclareLaunchArgument(
             "antenna_offset_deg",
             default_value="90.0",
@@ -47,20 +58,47 @@ def generate_launch_description():
                         "90.0 = antennas side-by-side (West=0 clockwise). "
                         "0.0 = antennas front-back (North=0 clockwise, standard NMEA)."
         ),
-        Node(
-            package="custom_localization",
-            executable="lg580p_driver_node",
-            name="lg580p_driver",
-            parameters=[
-                {
-                    "port": LaunchConfiguration("gps_port"),
-                    "baud": LaunchConfiguration("gps_baud"),
-                    "gps_frame": LaunchConfiguration("gps_frame"),
-                    "antenna_offset_deg": LaunchConfiguration("antenna_offset_deg"),
-                }
-            ],
-            output="screen",
+        # Node(
+        #     package="micro_ros_agent",
+        #     executable="micro_ros_agent",
+        #     name="imu_agent",
+        #     output="screen",
+            
+        #     parameters=[{
+        #         'serial',
+        #         '--dev', 
+        #         imu_port,
+        #         '-b', 
+        #         imu_baud
+        #     }
+              
+        #     ]
+        # ),
+        ExecuteProcess(
+        cmd=[
+            'ros2', 'run',
+            'micro_ros_agent', 'micro_ros_agent',
+            'serial',
+            '--dev', imu_port,
+            '-b', imu_baud,
+        ],
+        output='screen'
         ),
+
+        # Node(
+        #     package="custom_localization",
+        #     executable="lg580p_driver_node",
+        #     name="lg580p_driver",
+        #     parameters=[
+        #         {
+        #             "port": LaunchConfiguration("gps_port"),
+        #             "baud": LaunchConfiguration("gps_baud"),
+        #             "gps_frame": LaunchConfiguration("gps_frame"),
+        #             "antenna_offset_deg": LaunchConfiguration("antenna_offset_deg"),
+        #         }
+        #     ],
+        #     output="screen",
+        # ),
         Node(
             package='robot_localization',
             executable='navsat_transform_node',
@@ -82,4 +120,6 @@ def generate_launch_description():
 
             parameters=[ekf_file]
         )
+
+
     ])
